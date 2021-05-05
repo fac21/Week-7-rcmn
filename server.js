156fd95a9d80f8f085c9405e80c7607ed0215568
Nafisa Axmed <nafisa@Nafisas-MacBook-Air.local>
2021-05-05 14:10:26 +0100

    started sign up post function

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 
 const server = express();
-
+const PORT = process.env.PORT || 3000;
 const staticHandler = express.static("public");
 server.use(staticHandler);
 
 const cookieParser = require("cookie-parser");
-const bodyParser = express.urlencoded();
+const bodyParser = express.urlencoded({ extended: false });
+
+const signUp = require('./routes/signup')
 
 server.use(cookieParser(process.env.COOKIE_SECRET));
+server.use(bodyParser)
 
-const PORT = process.env.PORT || 3000;
+
+server.get("/signup", signUp.get)
+server.post("/signup", signUp.post)
+
 
 server.listen(PORT, () => console.log(`started on http://localhost:${PORT}`));
